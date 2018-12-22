a0d9f27901334311b942218d0227c339aba06cd0
CatsAreMLG <joshuadgon@gmail.com>
2018-12-21 20:53:29 -0600

    got images to display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./css/App.css";
 
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+const images = importAll(
+  require.context("./images/characters", false, /\.(png|jpe?g|svg)$/)
+);
+
+let arr = [];
+
+for (let i in images) {
+  arr.push(<img src={images[i]} alt="mario" />);
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {console.log(images)}
+        {arr}
       </div>
     );
   }
