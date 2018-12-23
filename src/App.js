003343c4e980a0b15b74d9aaed9307c38a1a33be
CatsAreMLG <joshuadgon@gmail.com>
2018-12-22 23:37:36 -0600

    added character component and state/prop passthrough

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 import "./css/App.css";
 import fighterNames from "./names";
+import Characters from "./components/characters/characters";
 
 function getKeyByValue(object, value) {
   return Object.keys(object).find(key => object[key] === value);
 }
 
 let newList = [];
+let arr = [];
 
 function importAll(r) {
   let images = {};
-  r.keys().map((item, index) => {
+  r.keys().map(item => {
     images[item.replace("./", "")] = r(item);
   });
   for (let i = 1; i <= Object.keys(fighterNames).length; i++) {
@@ -28,22 +30,29 @@ const images = importAll(
   require.context("./images/characters", false, /\.(png|jpe?g|svg)$/)
 );
 
-let arr = [];
-
 for (let i = 0; i < images.length; i++) {
   console.log(Object.keys(images[i])[0]);
   arr.push(
     <div className="image__container">
-      <span>{Object.keys(images[i])[0].split`.png`.join``.split`_`.map(e =>
-        e.toUpperCase()
-      ).join` `}</span>
+      <span className="characterName">{Object.keys(images[i])[0].split`.png`
+        .join``.split`_`.map(e => e.toUpperCase()).join` `}</span>
       <img src={Object.values(images[i])} alt={Object.keys(images[i])[0]} />
     </div>
   );
 }
 class App extends Component {
+  state = {
+    characters: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      characters: arr
+    });
+  }
+
   render() {
-    return <div className="App">{arr}</div>;
+    return <Characters characters={this.state.characters} />;
   }
 }
 
---
+++ src/components/characters/characters.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Characters = props => {
+  return <div className="App">{props.characters}</div>;
+};
+
+export default Characters;
