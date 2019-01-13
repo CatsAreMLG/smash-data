1219c26b36c412e7917a490983bb9214bb38e86f
CatsAreMLG <joshuadgon@gmail.com>
2019-01-13 16:56:57 -0600

    Displaying each character seperately

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -1,19 +1,7 @@
 import React from "react";
 
-const Characters = props => {
-  const more = e => {
-    document
-      .querySelectorAll(".App div:nth-child(n+30)")
-      .forEach(character => character.classList.add("shown"));
-    e.target.classList.toggle("hidden");
-  };
-
-  return (
-    <>
-      <div className="App">{props.characters}</div>
-      <button onClick={more}>See More</button>
-    </>
-  );
+const Character = props => {
+  return <>{props.children}</>;
 };
 
-export default Characters;
+export default Character;
--- src/components/characters/characters.js
+++ src/components/characters/characters.js
@@ -11,14 +11,12 @@ const Characters = props => {
   for (let i = 0; i < images.length; i++) {
     const style = { backgroundImage: `url(${Object.values(images[i])})` };
     arr.push(
-      <Character
-        key={`char-${i + 1}`}
-        style={style}
-        className="image__container"
-      >
-        <span className="characterName">{Object.keys(images[i])[0].split`.png`
-          .join``.split`_`.map(e => e.toUpperCase()).join` `}</span>
-        {/* <img src={Object.values(images[i])} alt={Object.keys(images[i])[0]} /> */}
+      <Character key={`char-${i + 1}`}>
+        <div style={style} className="image__container">
+          <span className="characterName">{Object.keys(images[i])[0].split`.png`
+            .join``.split`_`.map(e => e.toUpperCase()).join` `}</span>
+          {/* <img src={Object.values(images[i])} alt={Object.keys(images[i])[0]} /> */}
+        </div>
       </Character>
     );
   }
